36092adddc265c5b17184eedfefb6a64d27be6e8
fadelfebrian <fadelfebriann@gmail.com>
2019-08-02 13:41:57 +0700

    finish crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,60 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article,only: [:edit,:show,:update,:destroy] 
     
+    def index
+        @article = Article.all
+    end
+
     def new
-        @article = Article.new
+      @article = Article.new
     end
 
     def create
-        #render plain: params[:article].inspect
+        # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        # redirect_to article_show(@article) 
+        if @article.save
+            flash[:notice]= "Create article success"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 
+    def show
+    end
+
+    def edit 
+    end
+
+    def update 
+        if @article.update(article_params)
+            flash[:notice] = "Succesfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+        
+    end
+
+    def destroy
+        
+        if @article.destroy
+            flash[:notice] = "Deleted Succesfully"
+            redirect_to articles_path
+        end
+    end
+
+    def tes
+
+    end
+
+
     private
+        def set_article
+            @article = Article.find(params[:id])
+        end
+    
         def article_params
             params.require(:article).permit(:title,:description)
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  
+  root 'pages#home'
+  get  'about', to: 'pages#about'
 
-  root  'pages#home'
-  get  'about',to:'pages#about'
+  get 'tes', to: 'articles#tes'
 
-  resources:articles
-
+  resources :articles
 end
