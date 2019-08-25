3b81090e21d759173919381f5ae01dc12bc0cbb3
fadelfebrian <fadelfebriann@gmail.com>
2019-08-25 23:43:25 +0700

    create validation users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
